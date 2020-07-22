50345422bafc176f0ca86fcb9b581d50ba0315d7
Rohan Aggarwal <44223503+RohanAggarwal07@users.noreply.github.com>
2020-07-22 22:47:15 +0530

    final commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ModalModule} from 'ngx-bootstrap/modal';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import {NgxSpinnerModule} from 'ngx-spinner';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    InfiniteScrollModule,
+    NgxSpinnerModule,
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
